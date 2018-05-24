cb49366e0073a71bb037ad9e43141b60d7b53170
Vaishnavimd <vaishu.md@gmail.com>
2018-05-24 20:42:12 +0530

    added first example from the book

---
+++ exc1.1.cpp
@@ -0,0 +1,13 @@
+/*Exercise 1.1: Review the documentation for your compiler and determine what file
+naming convention it uses. Compile and run the main program from page 2.
+
+The GCC compiler is being used in this project
+GCC - GNU compiler collection
+Language supported by GNU - c, c++, Objective- C++, Fortron, Ada, Go and BRIG
+file name convention for GCC - cpp works fine
+*/
+
+int main()
+{
+return 0;
+}
